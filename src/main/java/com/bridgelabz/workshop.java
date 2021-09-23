4dd708b3f4755932ae2f37b44bd73b856a2e51b4
ravisingh45@gmail.com <81140145+ravisingh45@users.noreply.github.com>
2021-09-23 09:54:16 +0530

    ADDED:Choose the letter by user

--- src/main/java/com/bridgelabz/workshop.java
+++ src/main/java/com/bridgelabz/workshop.java
@@ -1,5 +1,7 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class workshop {
     public class TicTacToe {
 
@@ -9,10 +11,29 @@ public class workshop {
                 board[a] = ' ';
             }
         }
-
+        static char player,computer;
+        public  static char playerChoose(char CHOOSE_LETTER)
+        {
+            if(CHOOSE_LETTER == 'o'){
+                player ='o';
+                computer = 'x';
+            }
+            else if(CHOOSE_LETTER == 'x'){
+                player ='x';
+                computer = 'o';
+        }
+            else {
+                System.out.println("enter valid character");
+            }
+            return player;
+        }
         public static void main(String[] args) {
             System.out.println("Welcome to TIC-TAC-TOE BOARD");
             createEmptyBoard();
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Choose a letter x or o");
+            char CHOOSE_LETTER = scanner.next().charAt('o');
+            playerChoose(CHOOSE_LETTER);
         }
     }
 }
